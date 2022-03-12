aeb6dfe3b863546c9eb0f5922e77ad5e3190f328
Ira Horecka <ira89@icloud.com>
2022-03-12 12:41:22 -0500

    add setup.py for simple pypi deployment

---
+++ setup.py
@@ -0,0 +1,35 @@
+"""Setup for the natlparks package."""
+
+import setuptools
+
+
+with open("README.rst") as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Ira Horecka",
+    author_email="ira89@icloud.com",
+    name="python-natlparks",
+    license="MIT",
+    description="Simple API wrapper for US National Park Services",
+    version="v0.1.2",
+    long_description=README,
+    url="https://github.com/irahorecka/python-natlparks",
+    packages=["actransit"],
+    python_requires=">=3.5",
+    install_requires=["requests"],
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+    ],
+)
